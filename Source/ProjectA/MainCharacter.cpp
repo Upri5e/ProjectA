b979d81b76f234d9c1d41d5b8fac0a7ca558d8f3
Justice Neyedli <justicenarts@gmail.com>
2020-06-21 12:49:54 -0700

    Fixed Complie Error and added new componenet
    Moved #include maincharacter.h above other includes. Plus item pick up componenet

--- Source/ProjectA/MainCharacter.cpp
+++ Source/ProjectA/MainCharacter.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "MainCharacter.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Engine/World.h"
-#include "MainCharacter.h"
 
 // Sets default values
 AMainCharacter::AMainCharacter()
